a6a1d1f750f148475250d9fe7954a2e97a90989b
Marco Greiveldinger <43136849+marcogreiveldinger@users.noreply.github.com>
2023-07-27 16:22:31 +0800

    add metadata for seo friendliness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,59 @@ config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Marco Greiveldinger",
-  description: "My personal portfolio",
+  metadataBase: new URL("https://marco-greiveldinger.de"),
+  authors: { name: "Marco Greiveldinger" },
+  creator: "Marco Greiveldinger",
+  publisher: "Marco Greiveldinger",
+  title: {
+    template: "%s | Marco Greiveldinger",
+    default: "Marco Greiveldinger – Developer and Cloud Expert",
+  },
+  description:
+    "A freelance developer and cloud expert to help you build your architecture.",
+  keywords: [
+    "DevOps Engineer",
+    "Cloud Architecture",
+    "Web Development",
+    "Freelancer",
+  ],
+  openGraph: {
+    type: "profile",
+    firstName: "Marco",
+    lastName: "Greiveldinger",
+    username: "mgreivel",
+    emails: ["hello@marco-greiveldinger.de"],
+    url: "https://marco-greiveldinger.de",
+    siteName: "Marco Greiveldinger",
+    title: "Marco Greiveldinger – Developer and Cloud Expert",
+    description:
+      "A freelance developer and cloud expert to help you build your architecture.",
+    countryName: "Germany",
+    locale: "en_US",
+  },
+  viewport: { width: "device-width", initialScale: 1 },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    title: "Marco Greiveldinger",
+  },
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
